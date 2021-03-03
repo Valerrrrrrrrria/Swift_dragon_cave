44cf0ddc6192f04254bcba08f695f90f2666f090
valeriia <novikovavaleria994@gmail.com>
2021-03-03 10:51:53 +0300

    Privacy

---
+++ WebView/Privacy.swift
@@ -0,0 +1,21 @@
+//
+//  Privacy.swift
+//  1WIN
+//
+//  Created by Валерия Новикова on 02.03.2021.
+//  Copyright © 2021 Валерия Новикова. All rights reserved.
+//
+
+import UIKit
+
+class Privacy: UIWebView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
